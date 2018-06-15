53842a95fecc94566795d33567b9173b5d92a9d9
owenzhang2010 <owenzhang2010@berkeley.edu>
2018-06-15 12:28:29 -0400

    skeleton-ing ranking logic

--- src/HandRanker.java
+++ src/HandRanker.java
@@ -1,4 +1,18 @@
+import java.util.List;
+
 public class HandRanker {
+    private static final int HIGH_CARD = 0;
+    private static final int PAIR = 1;
+    private static final int TWO_PAIR = 2;
+    private static final int THREES = 3;
+    private static final int STRAIGHT = 4;
+    private static final int FLUSH = 5;
+    private static final int FULL_HOUSE = 6;
+    private static final int FOURS = 7;
+    private static final int STRAIGHT_FLUSH = 8;
+    private static final int ROYAL_FLUSH = 9;
+
+
     public static int rankHand(Card[] holeCards, Card[] tableCards) {
         if (holeCards == null || tableCards == null) throw new IllegalArgumentException("somethings null");
         if (holeCards.length != 2) throw new IllegalArgumentException("hole cards wrong size");
@@ -21,7 +35,42 @@ public class HandRanker {
     }
 
     private static int rankHand(Card[] fives) {
+        int[] rankFreqs = new int[15]; // position 0 is dummy, 1 and 14 are both for aces, others in between
+        int[] suitFreqs = new int[4]; // CDHS
+        String suits = "CDHS";
+
+        for (Card c : fives) {
+            rankFreqs[c.rank] += 1;
+            suitFreqs[suits.indexOf(c.suit)] += 1;
+        }
+        rankFreqs[1] = rankFreqs[14]; // aces
         return 0;
+    }
+
+    private static Card[] breakTies(List<Card[]> hands, int handType) { // returns null if it's a push
+        switch (handType) {
+            case HIGH_CARD:
+                return null;
+            case PAIR:
+                return null;
+            case TWO_PAIR:
+                return null;
+            case THREES:
+                return null;
+            case STRAIGHT:
+                return null;
+            case FLUSH:
+                return null;
+            case FULL_HOUSE:
+                return null;
+            case FOURS:
+                return null;
+            case STRAIGHT_FLUSH:
+                return null;
+            default:
+                return null;
+
+        }
     }
 
     // from https://stackoverflow.com/questions/29910312/algorithm-to-get-all-the-combinations-of-size-n-from-an-array-java
